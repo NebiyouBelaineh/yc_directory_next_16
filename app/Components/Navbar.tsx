a335e10d20382bd898a72bf238c2b39e6d06938e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-10-03 11:21:27 +0300

    feat(auth): add google oauth

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,5 +1,4 @@
 import { auth, signIn, signOut } from '@/auth'
-import { redirect } from 'next/dist/server/api-utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -18,27 +17,31 @@ const Navbar = async () => {
               <>
                 <Link href={"/startup/create"}>Create</Link>
 
+                {/* SignOut form action */}
                 <form action={async () => {
                   "use server";
-                  await signOut({redirectTo: "/"});
+                  await signOut({ redirectTo: "/" });
                 }}>
                   <button type='submit'><span>Logout</span></button>
                 </form>
-
-                <Link href={`/user/${session?.user?.id}`}>
+                <Link href={`/user/${session?.id}`}>
                   <span>{session?.user?.name}</span>
                 </Link>
               </>
             ) :
               <>
+                {/* SignIn form action */}
                 <form action={async () => {
                   "use server";
                   await signIn('github');
                 }}>
-                  <button type='submit'>
-                    <span>Login</span>
-                  </button>
-
+                  <button type='submit'><span>Login with Github</span></button>
+                </form>
+                <form action={async () => {
+                  "use server";
+                  await signIn('google');
+                }}>
+                  <button type='submit'><span>Login with Google</span></button>
                 </form>
               </>
           }
