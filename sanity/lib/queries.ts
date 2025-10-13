5af6b73681fb902f005b1798d185754f97389a94
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-10-13 18:14:20 +0300

    feat: add GROQ query for getting startups by id.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || category match $search || title match $search || author->name match $search ] | order(_createdAt desc) {
   _id,
   title,
   slug,
@@ -17,3 +17,23 @@ export const STARTUPS_QUERY =
   category,
   image
 }`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0] {
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id,
+    name,
+    image,
+    bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
+}
+  `);
