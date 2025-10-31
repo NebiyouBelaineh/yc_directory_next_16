9d130a512b78707c120a5a97b0ff0925857dfa41
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-10-31 21:27:21 +0300

    feat: enhance user profile to display personalized startup label based on session

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -2,9 +2,11 @@ import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_USER_ID, STARTUPS_BY_AUTHOR_ID } from "@/sanity/lib/queries";
 import Image from "next/image";
 import StartUpCard, { StartupTypeCard } from "./StartUpCard";
+import { auth } from "@/auth";
 
 const UserProfile = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
+  const session = await auth();
 
   const user = await client.fetch(AUTHOR_BY_USER_ID, {
     id,
@@ -33,7 +35,9 @@ const UserProfile = async ({ params }: { params: Promise<{ id: string }> }) => {
           <p className=".mt-1 text-center text-14-normal">{user?.bio}</p>
         </div>
         <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
-          <p className="text-30-bold">All Startups</p>
+          <p className="text-30-bold">
+            {(session?.id === user?._id ? "Your" : "All") + " Startups"}
+          </p>
           <ul className="card_grid-sm">
             {startups.map((s: StartupTypeCard) => (
               <StartUpCard post={s} key={s._id} />
