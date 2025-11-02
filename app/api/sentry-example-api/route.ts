6165cd0539735cf71e1ef497d376dab12c465fe7
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-11-02 20:40:58 +0300

    fix: enable dynamic API route and ensure SentryExampleAPIError is thrown correctly fix: simplify GlobalError component by removing unused NextError and adjusting error type fix: update Sentry feedback integration configuration in instrumentation-client

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 
-// export const dynamic = "force-dynamic";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const dynamic = "force-dynamic";
 class SentryExampleAPIError extends Error {
   constructor(message: string | undefined) {
     super(message);
@@ -10,6 +9,6 @@ class SentryExampleAPIError extends Error {
 }
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  // throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
+  throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
   return NextResponse.json({ data: "Testing Sentry Error..." });
 }
