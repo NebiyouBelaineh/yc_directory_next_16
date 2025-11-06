91ba625e409e72ad3f1caaa6ba48edfa2fdea228
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-11-06 13:57:50 +0300

    Comment out unused FeedbackButton component

--- components/FeedBackButton.tsx
+++ components/FeedBackButton.tsx
@@ -1,49 +1,49 @@
-"use client";
-import * as Sentry from "@sentry/nextjs";
-import { useEffect, useState } from "react";
-import { Button } from "./ui/button";
-import type { buildFeedbackIntegration } from "/home/neba/personal-projects/learning/next-js/yc_directory/node_modules/@sentry/nextjs/node_modules/@sentry-internal/feedback/build/npm/types/core/integration";
-type FeedbackIntegration = ReturnType<typeof buildFeedbackIntegration>;
+// "use client";
+// import * as Sentry from "@sentry/nextjs";
+// import { useEffect, useState } from "react";
+// import { Button } from "./ui/button";
+// // import type { buildFeedbackIntegration } from "/home/neba/personal-projects/learning/next-js/yc_directory/node_modules/@sentry/nextjs/node_modules/@sentry-internal/feedback/build/npm/types/core/integration";
+// // type FeedbackIntegration = ReturnType<typeof buildFeedbackIntegration>;
 
-type SentryWidget = {
-  removeFromDom: () => void;
-} | null | undefined;
+// type SentryWidget = {
+//   removeFromDom: () => void;
+// } | null | undefined;
 
-const FeedBackButton = () => {
-  const [feedback, setFeedback] = useState<ReturnType<FeedbackIntegration>>();
-  // Read `getFeedback` on the client only, to avoid hydration errors during server rendering
-  useEffect(() => {
-    setFeedback(Sentry.getFeedback());
-  }, []);
+// const FeedBackButton = () => {
+//   const [feedback, setFeedback] = useState();
+//   // Read `getFeedback` on the client only, to avoid hydration errors during server rendering
+//   useEffect(() => {
+//     setFeedback(Sentry.getFeedback());
+//   }, []);
 
-  const [widget, setWidget] = useState<SentryWidget>(null);
-  return (
-    <div className="rounded-2xl">
-      <Button
-        type="button"
-        className="bg-transparent hover:bg-transparent text-xl h-full"
-        onClick={async () => {
-          if (widget) {
-            widget.removeFromDom();
-            setWidget(null);
-          } else {
-            setWidget(feedback?.createWidget());
-          }
-        }}
-      >
-        {widget ? (
-          <div>
-            <p>Check right corner widget.</p>
-            <span className="underline rounded hover:text-primary hover:cursor-pointer">Click to hide bug report widget.</span>
-          </div>
-        ) : (
-          <div>
-            <span  className="underline rounded hover:text-primary hover:cursor-pointer">Want to report a bug?</span>
-          </div>
-        )}
-      </Button>
-    </div>
-  );
-};
+//   const [widget, setWidget] = useState<SentryWidget>(null);
+//   return (
+//     <div className="rounded-2xl">
+//       <Button
+//         type="button"
+//         className="bg-transparent hover:bg-transparent text-xl h-full"
+//         onClick={async () => {
+//           if (widget) {
+//             widget.removeFromDom();
+//             setWidget(null);
+//           } else {
+//             setWidget(feedback?.createWidget());
+//           }
+//         }}
+//       >
+//         {widget ? (
+//           <div>
+//             <p>Check right corner widget.</p>
+//             <span className="underline rounded hover:text-primary hover:cursor-pointer">Click to hide bug report widget.</span>
+//           </div>
+//         ) : (
+//           <div>
+//             <span  className="underline rounded hover:text-primary hover:cursor-pointer">Want to report a bug?</span>
+//           </div>
+//         )}
+//       </Button>
+//     </div>
+//   );
+// };
 
-export default FeedBackButton;
+// export default FeedBackButton;
