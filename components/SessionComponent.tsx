4b800034fbdeedee564c8c962ab4597433f6b646
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-11-11 18:37:45 +0300

    fix: add tooltips for login and logout, also add icon for logout

--- components/SessionComponent.tsx
+++ components/SessionComponent.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { IoCreate, IoLogOut } from "react-icons/io5";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 const SessionComponent = async () => {
   const session = await auth();
@@ -24,9 +25,18 @@ const SessionComponent = async () => {
                 await signOut({ redirectTo: "/" });
               }}
             >
-              <button type="submit">
-                <IoLogOut />
-              </button>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button type="submit">
+                    <IoLogOut />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className="hover:cursor-pointer bg-black text-white rounded p-1">
+                    Logout
+                  </span>
+                </TooltipContent>
+              </Tooltip>
             </form>
             <Link href={`/user/${session?.id}`}>
               <Avatar className="avatar">
@@ -48,12 +58,21 @@ const SessionComponent = async () => {
                 await signIn("github");
               }}
             >
-              <button
-                type="submit"
-                className="hover: cursor-pointer hover:bg-secondary border-1 rounded-lg border-black-100 p-3"
-              >
-                <FaGithub />
-              </button>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    type="submit"
+                    className="hover: cursor-pointer hover:bg-secondary border-1 rounded-lg border-black-100 p-3"
+                  >
+                    <FaGithub />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className="hover:cursor-pointer bg-black text-white rounded p-1">
+                    Login with Github
+                  </span>
+                </TooltipContent>
+              </Tooltip>
             </form>
             <form
               className=""
@@ -62,12 +81,21 @@ const SessionComponent = async () => {
                 await signIn("google");
               }}
             >
-              <button
-                type="submit"
-                className="hover: cursor-pointer hover:bg-secondary border-1 rounded-lg border-black-100 p-3"
-              >
-                <FaGoogle />
-              </button>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    type="submit"
+                    className="hover: cursor-pointer hover:bg-secondary border-1 rounded-lg border-black-100 p-3"
+                  >
+                    <FaGoogle />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className="hover:cursor-pointer bg-black text-white rounded p-1">
+                    Login with Google
+                  </span>
+                </TooltipContent>
+              </Tooltip>
             </form>
           </>
         )}
