d191656732292f4a200a8acdf06b99a2e6b74d74
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-11-11 15:27:52 +0300

    feat: add tooltip component and integrate it into FeedBackButton for improved user experience

--- components/FeedBackButton.tsx
+++ components/FeedBackButton.tsx
@@ -4,12 +4,17 @@
 import * as Sentry from "@sentry/nextjs";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { FaBug } from "react-icons/fa";
 // import type { buildFeedbackIntegration } from "/home/neba/personal-projects/learning/next-js/yc_directory/node_modules/@sentry/nextjs/node_modules/@sentry-internal/feedback/build/npm/types/core/integration";
 // type FeedbackIntegration = ReturnType<typeof buildFeedbackIntegration>;
 
-type SentryWidget = {
-  removeFromDom: () => void;
-} | null | undefined;
+type SentryWidget =
+  | {
+      removeFromDom: () => void;
+    }
+  | null
+  | undefined;
 
 const FeedBackButton = () => {
   const [feedback, setFeedback] = useState();
@@ -21,29 +26,41 @@ const FeedBackButton = () => {
   const [widget, setWidget] = useState<SentryWidget>(null);
   return (
     <div className="rounded-2xl">
-      <Button
-        type="button"
-        className="bg-transparent hover:bg-transparent text-xl h-full"
-        onClick={async () => {
-          if (widget) {
-            widget.removeFromDom();
-            setWidget(null);
-          } else {
-            setWidget(feedback?.createWidget());
-          }
-        }}
-      >
-        {widget ? (
-          <div>
-            <p></p>
-            <span className="underline rounded hover:text-primary hover:cursor-pointer text-lg">Check the right corner widget. Click here to hide bug report widget.</span>
-          </div>
-        ) : (
-          <div>
-            <span  className="underline rounded hover:text-primary hover:cursor-pointer">Want to report a bug? Click here.</span>
-          </div>
-        )}
-      </Button>
+      <Tooltip className="bg-white">
+        <TooltipTrigger asChild>
+          <Button
+            type="button"
+            variant={"outline"}
+            className="h-ful"
+            onClick={async () => {
+              if (widget) {
+                widget.removeFromDom();
+                setWidget(null);
+              } else {
+                setWidget(feedback?.createWidget());
+              }
+            }}
+          >
+            <FaBug className={"border-black-100"}/>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          {widget ? (
+              <div>
+                <span className="hover:cursor-pointer bg-black text-white rounded p-1">
+                  Check the right corner widget. Click here to hide bug report
+                  widget.
+                </span>
+              </div>
+            ) : (
+              <div>
+                <span className="hover:cursor-pointer bg-black text-white rounded p-1">
+                  Want to report a bug? Click here.
+                </span>
+              </div>
+            )}
+        </TooltipContent>
+      </Tooltip>
     </div>
   );
 };
