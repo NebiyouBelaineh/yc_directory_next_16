eb04cb2a077357bbe323a0f5c347bfd75320b328
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2025-11-14 16:48:02 +0300

    fix: fix responsiveness for category and views, improve colors for edit and delete buttons

--- components/PostDescription.tsx
+++ components/PostDescription.tsx
@@ -20,18 +20,17 @@ const PostDescription = async ({
       <h1 className="heading">{startup?.title}</h1>
       <p className="sub-heading !max-w-5xl">{startup.description}</p>
       {session && session?.id === startup.author?._id && (
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-3">
           <Link
-            className="flex gap-2 items-center mt-5 px-4 py-2 h-9 rounded-lg hover:bg-green-300 hover:text-black hover:font-bold bg-green-700 text-white sm:p-2 sm:text-sm shadow-md"
+            className="flex gap-2 items-center mt-5 px-4 py-2 h-9 rounded-lg hover:bg-green-700 hover:font-bold bg-black-100 text-white sm:p-2 sm:text-sm shadow-md hover:scale-105 hover:transition duration-300 ease-in-out transform"
             href={`/startup/edit/${startup?._id}`}
           >
             <Edit />
             Edit Pitch
           </Link>
-          <Button
-            className="flex gap-2 items-center mt-5 p-2 rounded-lg hover:bg-red-300 hover:text-black hover:font-bold bg-red-700 text-white sm:p-2 sm:text-sm shadow-md"
-          >
-            <MdDelete className="size-6"/>
+          {/* Delete Pitch */}
+          <Button className="flex gap-2 items-center mt-5 p-2 rounded-lg hover:bg-red-700 hover:font-bold bg-black-100 text-white sm:p-2 sm:text-sm shadow-md hover:scale-105 hover:transition duration-300 ease-in-out transform">
+            <MdDelete className="size-6" />
             Delete Pitch
           </Button>
         </div>
--- components/PostDetails.tsx
+++ components/PostDetails.tsx
@@ -10,8 +10,6 @@ import {
 import { Edit, Eye } from "lucide-react";
 import { after } from "next/server";
 import { writeClient } from "@/sanity/lib/write-client";
-import { Tooltip, TooltipContent } from "./ui/tooltip";
-import { TooltipTrigger } from "@radix-ui/react-tooltip";
 import { MdDelete } from "react-icons/md";
 import { auth } from "@/auth";
 
@@ -46,7 +44,7 @@ const PostDetails = async ({
         className="w-full h-auto rounded-xl"
       />
       <div className="space-y-5 mt-10 max-w-4xl mx-auto">
-        <div className="flex-between gap-5">
+        <div className="grid sm:flex-between gap-5">
           <Link
             href={`/user/${startup.author?._id}/`}
             className="flex gap-2 items-center mb-3"
@@ -67,13 +65,10 @@ const PostDetails = async ({
               </p>
             </div>
           </Link>
-          <div className="grid grid-cols-1 items-center gap-2 p-2">
-            <div className="flex items-center justify-center gap-2 bg-primary-100 rounded-full">
-              <Eye />
-              <p className="">
-                <span className="font-black"></span>
-                {`${views?.views || 0}`}
-              </p>
+          <div className="flex flex-between sm:grid sm:grid-cols-1 items-center gap-2 p-2">
+            <div className="flex items-center justify-center gap-2">
+              <Eye className="text-primary rounded-full" />
+              <p>{`${views?.views || 0}`}</p>
             </div>
             <p className="category-tag">{startup.category}</p>
           </div>
@@ -81,31 +76,15 @@ const PostDetails = async ({
         <div className="flex items-center gap-2">
           <h3 className="text-30-bold">Pitch Details</h3>
           {session && session.id === startup.author?._id && (
-            <div>
-              <Tooltip>
-                <TooltipTrigger>
-                  <Link href={`/startup/edit/${startup._id}`}>
-                    <Edit className="text-green-700 hover:size-7" />
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p className="text-black border border-primary-100 font-bold p-2 bg-white-100 text-sm flex items-center rounded-2xl">
-                    Edit your pitch
-                  </p>
-                </TooltipContent>
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger>
-                  <Link href={`/startup/edit/${startup._id}`}>
-                    <MdDelete className="text-red-700 size-6 hover:size-8" />
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p className="text-black border border-primary-100 font-bold p-2 bg-white-100 text-sm flex items-center rounded-2xl">
-                    Delete Pitch
-                  </p>
-                </TooltipContent>
-              </Tooltip>
+            <div className="flex items-center gap-3 pl-2">
+              {/* Edit Pitch */}
+              <Link href={`/startup/edit/${startup._id}`}>
+                <Edit className="text-black-100 hover:text-green-700 hover:size-7 hover:transition duration-300 ease-in-out transform" />
+              </Link>
+              {/* Delete Pitch */}
+              <button className="hover:text-red-700">
+                <MdDelete className="size-6 hover:size-8 hover:transition duration-300 ease-in-out transform" />
+              </button>
             </div>
           )}
         </div>
